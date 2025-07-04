e6ff26a290512398a289665ef64200b57727b7d2
Pranjal22874 <pranjalskotkar@eaton.com>
2025-07-04 11:38:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 
 
 pd_df=my_dataframe.to_pandas()
@@ -76,6 +76,6 @@ if ingredients_list:
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
     
       st.subheader(fruit_chosen + 'Nutrition Information')
-      fruityvice_response=requests.get("https://my.fruityvice.com/api/fruit/" +  search_on)
+      fruityvice_response=requests.get("https://my.fruityvice.com/api/fruit/" +  fruit_chosen)
       fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
