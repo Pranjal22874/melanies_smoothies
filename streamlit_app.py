4d18fef458ef213f4e14ef361c002c5c9343f3c3
Pranjal22874 <pranjalskotkar@eaton.com>
2025-07-04 09:31:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
                 # values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
                 #st.write(my_insert_stmt)
 import requests
-smoothiefroot_response=requests.get(https://my.smoothiefroot.com/api/fruit/watermelon)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
                 time_to_insert=st.button('Submit Order')
                 
